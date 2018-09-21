b44436fbb1a154a923ed5e9e101aa8cf87c178f6
Caleb <savershot685@gmail.com>
2018-09-21 08:21:33 -0500

    So, now none of the error handling works right, and neither does the hard mode setting, great

--- sscJS.js
+++ sscJS.js
@@ -11,15 +11,15 @@ const Noclone = "This game contains no duplicate cards...unless someone snuck th
 TownCon.addEventListener('click', ()=>{
     tcT.value= parseInt(tc1.value) + parseInt(tc2.value) + parseInt(tc3.value)
     //card out of bounds error
-    if(tc1.value >=19 || tc1.value <=0){
+    /*if(tc1.value > 18 || tc1.value < 1){
         alert(Icard)
         tcT.value = "XX"
     }
-    if(tc2.value >=19 || tc2.value <=0){
+    if(tc2.value > 18 || tc2.value < 1){
         alert(Icard)
         tcT.value = "XX"
     }
-    if(tc3.value >=19 || tc3.value <=0){
+    if(tc3.value > 18 || tc3.value < 1){
         alert(Icard)
         tcT.value = "XX"
     }
@@ -35,13 +35,32 @@ TownCon.addEventListener('click', ()=>{
     if(tc3.value == tc2.value || tc1.value){
         alert(Noclone)
         tcT.value = "XY"
-    }
+    }*/
 
 });
 District.addEventListener('click', ()=>{
     districtTotal.value=parseInt(residential.value)+ parseInt(industrial.value)+ parseInt(recreation.value)+ parseInt(comercial.value)
 });
 eGame.addEventListener('click', ()=>{
+    if(hardM.checked == true){
+        if(residential.value > industrial.value || recreation.value || comercial.value){
+            industrial.value = 0;
+            recreation.value = 0;
+            comercial.value = 0;
+        }else if (industrial.value > residential.value || recreation.value || comercial.value){
+            residential.value = 0;
+            recreation.value = 0;
+            comercial.value = 0;
+        }else if(recreation.value > industrial.value || residential.value || comercial.value){
+            industrial.value = 0;
+            residential.value = 0;
+            comercial.value = 0;
+        }else if(comercial.value > industrial.value || recreation.value || residential.value){
+            industrial.value = 0;
+            recreation.value = 0;
+            comercial.value =0;
+        }else{}
+    }
     EGS.value = districtTotal.value - parseInt(roadCount.value)+ parseInt(wrecker.value)+ parseInt(consI.value)+ parseInt(poiB.value)
     sRate.value = (EGS.value /tcT.value)* 100
 });
